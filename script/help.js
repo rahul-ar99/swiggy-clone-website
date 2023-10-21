edc312c6aa03080652f05c233c92f3476b1a771c
rahul-ar99 <rahulararukattu@gmail.com>
2023-10-21 17:15:51 +0530

    changes in html

--- script/help.js
+++ script/help.js
@@ -140,10 +140,11 @@ for(let i=0;i<helpHead.length;i++){
     helpHead[i].addEventListener("click",function(){
         this.classList.toggle("active");
         var content = this.nextElementSibling;
-        if(content.style.display ==="block"){
+        if(content.style.display ==="flex"){
             content.style.display = "none";
         } else {
-            content.style.display = "block"
+            content.style.display = "flex"
         }
     })
 }
+
